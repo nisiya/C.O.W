465ae97efe733964258f1c12b07e32577e651eff
nisiya <syni679@gmail.com>
2018-02-02 00:46:40 -0500

    implemented function to remove comments

--- distrib/lexer.js
+++ distrib/lexer.js
@@ -9,25 +9,59 @@ var Compiler;
     var Lexer = /** @class */ (function () {
         function Lexer() {
         }
-        // public init(): void {
-        // }
+        Lexer.prototype.init = function () {
+            this.currentLine = 0;
+            this.currentColumn = 0;
+        };
         Lexer.getInput = function (btn) {
-            var userPrg = editor.getValue();
-            var charCode = new Array();
-            // var commentRegEx = /\/\*a/;
-            // var userPrgClean = commentRegEx.test(userPrg);
-            // var output = <HTMLInputElement> document.getElementById("test"); 
-            // output.value = userPrgClean.toString();
-            // for (var i=0; i<userPrg.length; i++){
-            //   charCode.push(userPrg.charCodeAt(i));
-            // }
-            this.removeComments(userPrg);
+            // var userPrg = editor.getValue();
+            var input = document.getElementById("input");
+            var userPrg = new Array();
+            userPrg = input.value.split('');
+            var userPrgClean = this.removeComments(userPrg);
         };
         Lexer.removeComments = function (userPrg) {
-            var commentRegEx = /\/\*(.*?(\r\n)*?)\*\//;
-            var userPrgClean = userPrg.replace(commentRegEx, ' ');
-            var output = document.getElementById("test");
-            output.value = userPrgClean.toString();
+            // let commentStart:RegExp = /(\/\*)/;
+            // let commentEnd:RegExp = /(\*\/)/;
+            // let start:number = userPrg.search(commentStart);
+            // console.log("start " + start);
+            // let end:number = userPrg.search(commentEnd);
+            // let beforeComment = userPrg.slice(0,start);
+            // let afterComment = userPrg.slice(end+1, userPrg.length);
+            // while (start != -1){
+            //   while (start <= end){
+            //     if(userPrg.charCodeAt(start) == 10){
+            //       start++;
+            //     } else{
+            //       userPrg[start] = ' ';
+            //       start++;
+            //     }
+            //   }
+            //   userPrg.search(commentStart);
+            // }
+            // var commentRegEx = /(\/\*)(\s|\S)*(\*\/)/;
+            // var commentRegEx2 = /(\*\/)/;
+            // var userPrgClean = userPrg.search(commentRegEx);
+            // var userPrgClean = userPrg.search(commentRegEx2);
+            // var output = <HTMLInputElement> document.getElementById("test"); 
+            // output.value = userPrg.toString();
+            // return userPrg.toString();
+            for (var i = 0; i < userPrg.length - 1; i++) {
+                if (userPrg[i] == '/' && userPrg[i + 1] == '*') {
+                    userPrg[i] = ' ';
+                    userPrg[i + 1] = ' ';
+                    i += 2;
+                    while (userPrg[i] != '*' && userPrg[i + 1] != '/') {
+                        userPrg[i] = ' ';
+                        i++;
+                    }
+                    userPrg[i] = ' ';
+                    userPrg[i + 1] = ' ';
+                    i += 2;
+                }
+            }
+            console.log(userPrg);
+            return userPrg;
         };
         return Lexer;
     }());
--- source/lexer.ts
+++ source/lexer.ts
@@ -9,25 +9,62 @@ Requires global.ts.
 module Compiler {
 
   export class Lexer {
-    // public init(): void {
-      // }
-    public static getInput(btn): void {
-      var userPrg = editor.getValue();
-      var charCode = new Array<number>();
-      // var commentRegEx = /\/\*a/;
-      // var userPrgClean = commentRegEx.test(userPrg);
-      // var output = <HTMLInputElement> document.getElementById("test"); 
-      // output.value = userPrgClean.toString();
-      // for (var i=0; i<userPrg.length; i++){
-      //   charCode.push(userPrg.charCodeAt(i));
-      // }
-      this.removeComments(userPrg);
+    public currentLine;
+    public currentColumn;
+
+    public init(): void {
+      this.currentLine = 0;
+      this.currentColumn = 0;
     }
-    public static removeComments(userPrg): void {
-      var commentRegEx = /\/\*(.*?(\r\n)*?)\*\//;
-      var userPrgClean = userPrg.replace(commentRegEx, ' ');
-      var output = <HTMLInputElement> document.getElementById("test"); 
-      output.value = userPrgClean.toString();
+    public static getInput(btn): void {
+      // var userPrg = editor.getValue();
+      let input = <HTMLInputElement> document.getElementById("input"); 
+      let userPrg = new Array<string>();
+      userPrg = input.value.split('');
+      let userPrgClean:string[] = this.removeComments(userPrg);
+    }
+    public static removeComments(userPrg): string[] {
+      // let commentStart:RegExp = /(\/\*)/;
+      // let commentEnd:RegExp = /(\*\/)/;
+      // let start:number = userPrg.search(commentStart);
+      // console.log("start " + start);
+      // let end:number = userPrg.search(commentEnd);
+      // let beforeComment = userPrg.slice(0,start);
+      // let afterComment = userPrg.slice(end+1, userPrg.length);
+      // while (start != -1){
+      //   while (start <= end){
+      //     if(userPrg.charCodeAt(start) == 10){
+      //       start++;
+      //     } else{
+      //       userPrg[start] = ' ';
+      //       start++;
+      //     }
+      //   }
+      //   userPrg.search(commentStart);
+      // }
+      // var commentRegEx = /(\/\*)(\s|\S)*(\*\/)/;
+      // var commentRegEx2 = /(\*\/)/;
+      // var userPrgClean = userPrg.search(commentRegEx);
+      // var userPrgClean = userPrg.search(commentRegEx2);
+      // var output = <HTMLInputElement> document.getElementById("test"); 
+      // output.value = userPrg.toString();
+      // return userPrg.toString();
+      for (var i=0; i<userPrg.length-1; i++){
+        if (userPrg[i]=='/' && userPrg[i+1]=='*'){
+          userPrg[i] = ' ';
+          userPrg[i+1] = ' ';
+          i+=2;
+          while (userPrg[i]!='*' && userPrg[i+1]!='/'){
+            userPrg[i] = ' ';
+            i++;
+          }
+          userPrg[i] = ' ';
+          userPrg[i+1] = ' ';
+          i+=2;
+        }
+      }
+      console.log(userPrg);
+      return userPrg;
     }
   }
 }
